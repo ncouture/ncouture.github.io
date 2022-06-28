de292cf3e1c5e0fc0f15bca888e3248df4a52fd8
Nicolas Couture <nicolas.couture@gmail.com>
2022-06-28 13:22:58 -0400

    adding worg

---
+++ worg/.dir-locals.el
@@ -0,0 +1,12 @@
+((nil
+  (indent-tabs-mode . t)
+  (tab-width . 8)
+  (fill-column . 70)
+  (sentence-end-double-space . t))
+ (org-mode
+  (indent-tabs-mode)
+  (org-edit-src-content-indentation . 0)
+  (org-adapt-indentation)
+  (org-list-two-spaces-after-bullet-regexp)
+  (org-list-description-max-indent . 5)
+  (org-blank-before-new-entry . ((heading . auto) (plain-list-item . auto)))))
